a4e34587def5c1e01c42364ee47b95fa723df05b
baltz <baltazar.parra@fliper.app>
2019-08-31 14:14:41 -0300

    Updates

---
+++ precache-manifest.87686a08994039d4bfc011fca2ce05d5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d1bdcd3a540139b5e09da4767734f0",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "bcdc65a44293db367c3a",
+    "url": "/pics/static/js/2.171233c8.chunk.js"
+  },
+  {
+    "revision": "4d75812525ab7435b38d",
+    "url": "/pics/static/js/main.e2751459.chunk.js"
+  },
+  {
+    "revision": "5c4f8c7925e6395a6e98",
+    "url": "/pics/static/js/runtime~main.bd145e01.js"
+  }
+]);
